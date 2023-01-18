d62ed6ab48177dab84390bb2486075f02ec00619
Ahmed Ayob abdo ali Ayob <108896341+thecoder512@users.noreply.github.com>
2023-01-18 20:32:36 +0200

    create index .html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,49 @@
-// logo anmation
-const logo = document.querySelector(".header-logo");
+// // logo anmation
+// const logo = document.querySelector(".header-logo");
 
-window.addEventListener("load", () => {
-  logo.classList.add("logo-animation");
-});
+// window.addEventListener("load", () => {
+//   logo.classList.add("logo-animation");
+// });
 
-// menu active btn
-const navLinks = document.querySelectorAll(".list-link");
+// // menu active btn
+// const navLinks = document.querySelectorAll(".list-link");
 
-function activeLink() {
-  navLinks.forEach((e) => {
-    e.classList.remove("focused-nav-link");
-    this.classList.add("focused-nav-link");
-  });
-}
+// function activeLink() {
+//   navLinks.forEach((e) => {
+//     e.classList.remove("focused-nav-link");
+//     this.classList.add("focused-nav-link");
+//   });
+// }
 
-navLinks.forEach((i) => {
-  i.addEventListener("click", activeLink);
-});
+// navLinks.forEach((i) => {
+//   i.addEventListener("click", activeLink);
+// });
 
 // // theme anmation
-const themeIcon = document.querySelector(".theme-icon");
+const themeIcon = document.querySelector("#theme-icon");
 const body = document.querySelector("body");
 
 themeIcon.addEventListener("click", () => {
   body.classList.toggle("theme-light");
   if (body.classList.contains("theme-light")) {
-    themeIcon.classList = `bx bx-moon theme-icon`;
+    themeIcon.classList = `uil uil-sun`;
   } else {
-    themeIcon.classList = `bx bx-sun theme-icon`;
+    themeIcon.classList = `uil uil-moon`;
   }
+});
+
+// toggle anmation for menu
+const toggleIcon = document.querySelector("#toggel-icon"),
+  navMenu = document.querySelector("#nav-menu");
+
+toggleIcon.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu-active");
+});
+
+//menu remove when link clicked
+const aehmd = document.querySelectorAll(".list-link");
+aehmd.forEach((element) => {
+  element.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu-active");
+  });
 });
